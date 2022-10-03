4ed98eb557d00aed44bf3458f18402c351d73c23
Taylor Smith <taylor.l.smith07@gmail.com>
2022-10-03 15:34:48 -0700

    added page for urls and urls id

--- express_server.js
+++ express_server.js
@@ -19,6 +19,16 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
