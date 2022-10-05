1d4a192ec3fa2c1d3f5dd2f3411173769e608966
Taylor Smith <taylor.l.smith07@gmail.com>
2022-10-04 17:17:33 -0700

    added logout function that clears cookie

--- express_server.js
+++ express_server.js
@@ -4,6 +4,7 @@ const PORT = 8080;
 const morgan = require('morgan');
 morgan(':method :url :status :res[content-length] - :response-time ms')
 app.set("view engine", "ejs");
+var cookieParser = require('cookie-parser')
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -11,6 +12,7 @@ const urlDatabase = {
 };
 
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 
 function generateRandomString(n) {
   let randomString           = '';
@@ -33,19 +35,46 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    username: req.cookies["username"], 
+    urls: urlDatabase 
+  };
   res.render("urls_index", templateVars);
 });
 
-app.post("/urls/:id/update", (req, res) => {
-  let newURL = req.body;
-  let id = req.params.id
-  urlDatabase[id] = newURL.newURL;
-  res.redirect("/urls");
-})
-
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+});
+
+app.get("/u/:id", (req, res) => {
+  let shortId = req.params.id
+  const longURL = urlDatabase[shortId];
+  res.redirect(longURL);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = {
+    username: req.cookies["username"], 
+    id: req.params.id, 
+    longURL: urlDatabase[req.params.id] 
+  };
+  res.render("urls_show", templateVars);
+});
+
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username)
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username")
+  res.redirect("/urls");
+});
+
+app.post("/urls/:id/update", (req, res) => {
+  let id = req.params.id
+  urlDatabase[id] = req.body.newURL;
+  res.redirect("/urls");
 });
 
 app.post("/urls", (req, res) => {
@@ -59,17 +88,6 @@ app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[id];
   res.redirect("/urls") 
 })
-
-app.get("/u/:id", (req, res) => {
-  let shortId = req.params.id
-  const longURL = urlDatabase[shortId];
-  res.redirect(longURL);
-});
-
-app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
-  res.render("urls_show", templateVars);
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
