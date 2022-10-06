e051348f3731828cb77fbd773b4e9fd1a32584ab
Taylor Smith <taylor.l.smith07@gmail.com>
2022-10-06 13:29:59 -0700

    added hashed passwords with bcrypt

--- express_server.js
+++ express_server.js
@@ -4,21 +4,28 @@ const PORT = 8080;
 const morgan = require('morgan');
 morgan(':method :url :status :res[content-length] - :response-time ms')
 app.set("view engine", "ejs");
+const bcrypt = require("bcryptjs");
 var cookieParser = require('cookie-parser')
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: {
+  longURL: "http://www.lighthouselabs.ca",
+  userId: "a",
+  },
+  '9sm5xK': {
+    longURL: "http://www.google.com",
+    userId: "a",
+  },
 };
 
 const users = {
-  1: {
-    id: 1,
+  a: {
+    id: "a",
     email: "tay@gmail.com",
     password: "1234"
   },
-  2: {
-    id: 2,
+  b: {
+    id: "b",
     email: "fake@fake.com",
     password: "pass"
   }
@@ -51,53 +58,25 @@ const getUserByEmail = (email) => {
 const generateUId = () => {
   return Math.random().toString(36).substring(2, 6);
 };
-
-
-app.get("/urls/new", (req, res) => {
-  userId = req.cookies["userId"];
-  const templateVars = {
-    user: users[userId], 
-    urls: urlDatabase 
-  };
-  res.render("urls_new", templateVars);
-});
-
-app.get("/urls/:id", (req, res) => {
-  const templateVars = {
-    userId: req.cookies["userId"], 
-    id: req.params.id, 
-    longURL: urlDatabase[req.params.id] 
-  };
-  res.render("urls_show", templateVars);
-});
-
-app.get("/u/:id", (req, res) => {
-  let shortId = req.params.id
-  const longURL = urlDatabase[shortId];
-  userId = req.cookies["userId"];
-  const templateVars = {
-    user: users[userId], 
-    urls: urlDatabase 
-  };
-  res.redirect(longURL, templateVars);
-});
-
-app.get("/urls", (req, res) => {
-  userId = req.cookies["userId"];
-  const templateVars = {
-    user: users[userId], 
-    urls: urlDatabase 
-  };
-  res.render("urls_index", templateVars);
-});
-
+const urlsForUser = (id) => {
+  let userURL = {}
+  for (const shortUrl in urlDatabase) {
+    if (urlDatabase[shortUrl].userId === id) {
+      userURL[shortUrl] = urlDatabase[shortUrl];
+    }
+  }
+  return userURL;
+}
 app.get("/register", (req, res) => {
   userId = req.cookies["userId"];
   const templateVars = {
     user: users[userId], 
     urls: urlDatabase 
   };
-  res.render("registration", templateVars)
+  if (!userId) {
+    return res.render("registration", templateVars)
+  }
+  res.redirect("urls")
 })
 
 app.get("/login", (req, res) => {
@@ -106,8 +85,70 @@ app.get("/login", (req, res) => {
     user: users[userId], 
     urls: urlDatabase 
   };
-  res.render("login", templateVars)
+  if (!userId) {
+    return res.render("login", templateVars)  
+  }
+  res.redirect("/urls")
 });
+
+app.get("/urls/new", (req, res) => {
+  userId = req.cookies["userId"];
+  const templateVars = {
+    user: users[userId], 
+    urls: urlsForUser(userId) 
+  };
+  if (!userId) {
+    res.redirect("/login");
+  }
+  res.render("urls_new", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  userId = req.cookies["userId"]
+  let shortId = req.params.id
+  const templateVars = {
+    user: users[userId], 
+    id: req.params.id, 
+    longURL: urlDatabase[req.params.id].longURL 
+  };
+  if (!userId) {
+    res.redirect("/login");
+  }
+  let userUrls = urlsForUser(userId)
+  for (let url in userUrls) {
+    if (url === shortId) {
+      res.render("urls_show", templateVars);
+    }
+  }
+  return res.send("That URL does not belong to you")
+});
+
+app.get("/u/:id", (req, res) => {
+  let shortId = req.params.id
+  const longURL = urlDatabase[shortId].longURL;
+  userId = req.cookies["userId"];
+  const templateVars = {
+    user: users[userId], 
+    urls: urlsForUser(userId) 
+  };
+  if (!urlDatabase[shortId]) {
+    return res.send("That ID is not in the short URL database")
+  }
+  res.redirect(longURL, templateVars);
+});
+
+app.get("/urls", (req, res) => {
+  userId = req.cookies["userId"];
+  const templateVars = {
+    user: users[userId], 
+    urls: urlsForUser(userId) 
+  };
+  if (!userId) {
+    res.send("Must log in before you can view your URLs");
+  }
+  res.render("urls_index", templateVars);
+});
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -122,6 +163,7 @@ app.get("/hello", (req, res) => {
 app.post('/register', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
+  const hashedPassword = bcrypt.hashSync(password, 10);
 
   if (!email || !password) {
     return res.status(400).send('please include email AND password');
@@ -136,17 +178,36 @@ app.post('/register', (req, res) => {
   const user = {
     id,
     email,
-    password
+    hashedPassword
   };
 
   users[id] = user;
   res.cookie('userId', user.id);
   res.redirect('/urls');
+  console.log(users)
 });
+
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  if (!email || !password) {
+    return res.status(403).send('please include email AND password');
+  }
+  const user = getUserByEmail(email);
+  if (!user) {
+    return res.status(403).send('No user with that email found');
+  }
+  if(!bcrypt.compareSync(password, user.hashedPassword)) {
+    return res.status(403).send("Ah ah ah, you didn't type the magic word");
+  }
+  res.cookie('userId', user.id);
+  res.redirect("/urls");
+});
+
 
 app.post("/urls/:id/update", (req, res) => {
   let id = req.params.id
-  urlDatabase[id] = req.body.newURL;
+  urlDatabase[id].longURL = req.body.newURL;
   res.redirect("/urls");
 });
 
@@ -156,31 +217,23 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect("/urls") 
 })
 
-app.post("/login", (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  if (!email || !password) {
-    return res.status(400).send('please include email AND password');
-  }
-  const user = getUserByEmail(email);
-  if (!user) {
-    return res.status(403).send('No user with that email found');
-  }
-  if (user.password !== password) {
-    return res.status(403).send("Ah ah ah, you didn't type the magic word");
-  }
-  res.cookie('userId', user.id);
-  res.redirect("/urls");
-});
 
 app.post("/logout", (req, res) => {
   res.clearCookie("userId")
-  res.redirect("/urls");
+  res.redirect("/login");
 });
 
 app.post("/urls", (req, res) => {
+  userId = req.cookies["userId"];
+  if (!userId) {
+    return res.send("Cannot shorten URLs unless you are signed in")
+  }
   let newId = generateRandomString(6);
-  urlDatabase[newId] = req.body.longURL;
+  urlDatabase[newId] = {
+    longURL: req.body.longURL,
+    userId: userId
+  }
+  console.log(urlDatabase)
   res.redirect(`/urls/${newId}`); 
 });
 
